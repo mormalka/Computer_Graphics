24735cf5252c81552f3be28e9241ef6f37c3c067
mormalka <morm4332@gmail.com>
2020-06-13 15:02:28 +0300

    front bumper implemented with notes -TODO

--- OpenGL_Model/src/edu/cg/models/Car/Front.java
+++ OpenGL_Model/src/edu/cg/models/Car/Front.java
@@ -1,17 +1,21 @@
 package edu.cg.models.Car;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import com.jogamp.opengl.GL2;
 import edu.cg.models.BoundingSphere;
 import edu.cg.models.IIntersectable;
 import edu.cg.models.IRenderable;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Front implements IRenderable, IIntersectable {
 	// TODO: Add necessary fields (e.g. the bumper).
 	private FrontHood hood = new FrontHood();
 	private PairOfWheels wheels = new PairOfWheels();
+
+	//new front bumber feature
+	private FrontBumber frontBumber = new FrontBumber();
+
 
 	@Override
 	public void render(GL2 gl) {
@@ -21,11 +25,22 @@ public class Front implements IRenderable, IIntersectable {
 		// Render hood - Use Red Material.
 		gl.glTranslated(-Specification.F_LENGTH / 2.0 + Specification.F_HOOD_LENGTH / 2.0, 0.0, 0.0);
 		hood.render(gl);
+
 		// Render the wheels.
 		gl.glTranslated(Specification.F_HOOD_LENGTH / 2.0 - 1.25 * Specification.TIRE_RADIUS,
 				0.5 * Specification.TIRE_RADIUS, 0.0);
 		wheels.render(gl);
+
+		// Render front bumber
+		gl.glTranslated(0.16875, -0.0375, 0.0); //TODO
+		//gl.glTranslated(2.0*Specification.TIRE_RADIUS, 0,0);
+		frontBumber.render(gl);
+
 		gl.glPopMatrix();
+
+
+
+
 	}
 
 	@Override
--- OpenGL_Model/src/edu/cg/models/Car/FrontBumber.java
+++ OpenGL_Model/src/edu/cg/models/Car/FrontBumber.java
@@ -1,17 +1,55 @@
 package edu.cg.models.Car;
 
 import com.jogamp.opengl.GL2;
-
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
 import edu.cg.models.IRenderable;
+import edu.cg.models.SkewedBox;
 
 public class FrontBumber implements IRenderable {
-	// TODO: Add fields as you like (and methods if you think they are necessary).
+	SkewedBox f_b_base;
+	SkewedBox f_b_wing;
+
 
 	@Override
 	public void render(GL2 gl) {
-		// TODO: Render the front bumper relative to it's local coordinate system.
 		// Remember the dimensions of the bumper, this is important when you
 		// combine the bumper with the hood.
+
+		gl.glPushMatrix(); //remember origin position
+		// Initialize quad
+		GLU glu = new GLU();
+		GLUquadric quad = glu.gluNewQuadric();
+
+		// Draw the center skewed box (the base without the wings)
+		Materials.SetBlackMetalMaterial(gl);
+		this.f_b_base  = createBase();
+		this.f_b_base.render(gl);
+
+		//Draw first wing
+		Double depth = Specification.F_BUMPER_WINGS_DEPTH / 2 + Specification.F_BUMPER_DEPTH / 2;
+		gl.glTranslated(0,0,depth);
+		gl.glPushMatrix(); // remember the first wing position
+		this.f_b_wing  = createWing(gl,glu,quad);
+		this.f_b_wing.render(gl);
+
+		//Draw sphere
+		renderSphere(gl,glu,quad);
+		gl.glPopMatrix(); //return to first wing position
+
+		//Draw second wing
+		gl.glTranslated(0,0,-2*depth);
+		gl.glPushMatrix();// remember the second wing position
+		this.f_b_wing  = createWing(gl,glu,quad);
+		this.f_b_wing.render(gl);
+
+		//Draw sphere
+		renderSphere(gl,glu,quad);
+		gl.glPopMatrix();//return to second wing position
+
+		gl.glPopMatrix(); //return to origin
+		glu.gluDeleteQuadric(quad); //Clear from memory
+
 	}
 
 	@Override
@@ -23,4 +61,31 @@ public class FrontBumber implements IRenderable {
 		return "FrontBumper";
 	}
 
+
+	private SkewedBox createBase(){
+		double h1 = Specification.F_BUMPER_HEIGHT_1;
+		double h2 = Specification.F_BUMPER_HEIGHT_2;
+		double length = Specification.F_BUMPER_LENGTH;
+		double depth = Specification.F_BUMPER_DEPTH;
+
+		return new SkewedBox(length,h1,h2,depth,depth);
+	}
+	private SkewedBox createWing(GL2 gl, GLU glu, GLUquadric quad){
+		Materials.SetBlackMetalMaterial(gl);
+		double h1 = Specification.F_BUMPER_WINGS_HEIGHT_1;
+		double h2 = Specification.F_BUMPER_WINGS_HEIGHT_2;
+		double length = Specification.F_BUMPER_LENGTH;
+		double depth = Specification.F_BUMPER_WINGS_DEPTH;
+
+		return new SkewedBox(length,h1,h2,depth,depth);
+	}
+
+	private void renderSphere(GL2 gl, GLU glu, GLUquadric quad) {
+		gl.glColor3d(1, 0, 0);
+		double sphere_point = Specification.F_BUMPER_WINGS_DEPTH / 2;
+		gl.glTranslated(0.0, 0.03937500000000001, 0.0); //TODO
+		glu.gluSphere(quad, 0.03200000000000001, 25, 25); //TODO
+	}
 }
+
+
